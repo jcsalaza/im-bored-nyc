5142cb4bddffb09e2f420c73c0fe051ef4ccaa1b
Jeremy Salazar <jeremysalazar09@gmail.com>
2014-09-10 16:11:07 -0400

    Edited user model and migration for redundancy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, :username, :email, :password, presence: :true
-  validates :username, :email, uniqueness: :true, on: :create
+  validates :name, :email, :password, presence: :true
+  validates :email, uniqueness: :true, on: :create
   validates :password, length: {minimum: 8, message: 'Must be at least 8 characters.'}
 end
